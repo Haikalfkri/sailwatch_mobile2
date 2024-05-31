b8b97f3787a1a486eeb37ce452e2302090b2a6e5
Jhonatan <jhonatansdbtr1705@gmail.com>
2024-05-31 16:07:11 +0700

    test

--- lib/Pages/Homepage/home_page.dart
+++ lib/Pages/Homepage/home_page.dart
@@ -10,7 +10,7 @@ import 'package:sailwatch_mobile/services/weatherServices.dart';
 
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -19,7 +19,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final XmlToJsonService xmlToJsonService = XmlToJsonService();
   late Future<Map<String, dynamic>> jsonData;
-  
+
   @override
   void initState() {
     super.initState();
@@ -31,122 +31,127 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          "Batam",
-                          style: GoogleFonts.inter(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                              color: Colors.white),
-                        ),
-                        Text(
-                          "May, 28 2024",
-                          style: GoogleFonts.inter(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 15,
-                              color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Column(
+          margin: const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
+          child: FutureBuilder<Map<String, dynamic>>(
+            future: jsonData,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(child: Text('Tidak ada data tersedia'));
+              } else {
+                try {
+                  var area = snapshot.data!['data']['forecast']['area'];
+                  var weatherData = area[0]['parameter'];
+                  var cityName = area[0]['name'][0]['\$t'];
+                  var temperature = weatherData[5]['timerange'][0]['value'][0]['\$t'];
+                  var windSpeed = weatherData[8]['timerange'][0]['value'][0]['\$t'];
+                  var humidity = weatherData[6]['timerange'][0]['value'][0]['\$t'];
+                  var hourlyWeather = weatherData[5]['timerange'];
+                  
+                  return Column(
                     children: [
-                      Image.asset(
-                        "assets/images/sunny.png",
-                        width: 250,
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Cerah",
-                        style: GoogleFonts.inter(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                          color: Colors.white,
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              cityName,
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              "May, 28 2024",  // Ubah sesuai kebutuhan
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 15,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
                         ),
-                      )
+                      ),
+                      const SizedBox(height: 20),
+                      Column(
+                        children: [
+                          Image.asset(
+                            "assets/images/sunny.png",
+                            width: 250,
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            "Cerah",
+                            style: GoogleFonts.inter(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      WeatherDetails(
+                        temperature: "$temperature°C",
+                        windSpeed: "$windSpeed km/h",
+                        humidity: "$humidity%",
+                      ),
+                      const SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Today",
+                            style: GoogleFonts.inter(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const ForecastPage(),
+                              ));
+                            },
+                            child: Text(
+                              "View Full Forecast",
+                              style: GoogleFonts.inter(
+                                fontSize: 14,
+                                color: const Color(0xFF1d86de),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: hourlyWeather.map<Widget>((hourData) {
+                            var time = hourData['datetime'];
+                            var temp = hourData['value'][0]['\$t']; // Simpan sebagai string
+                            var iconPath = "assets/images/sunny.png"; // Logika untuk ikon bisa ditambahkan di sini
+
+                            return HourlyWeatherItem(
+                              time: "$time",
+                              temperature: "$temp°C", // Gunakan langsung sebagai string
+                              iconPath: iconPath,
+                            );
+                          }).toList(),
+                        ),
+                      ),
                     ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              WeatherDetails(
-                temperature: "32C",
-                windSpeed: "10 km/h",
-                humidity: "85%",
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Today",
-                    style: GoogleFonts.inter(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const forecastPage()));
-                    },
-                    child: Text(
-                      "View Full Forecast",
-                      style: GoogleFonts.inter(
-                          fontSize: 14,
-                          color: Color(0xFF1d86de),
-                          fontWeight: FontWeight.w500),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    HourlyWeatherItem(
-                      time: "7:00",
-                      temperature: "32°C",
-                      iconPath: "assets/images/overcast.png",
-                    ),
-                    HourlyWeatherItem(
-                      time: "10:00",
-                      temperature: "32°C",
-                      iconPath: "assets/images/sunny.png",
-                    ),
-                    HourlyWeatherItem(
-                      time: "10:00",
-                      temperature: "32°C",
-                      iconPath: "assets/images/sunny.png",
-                    ),
-                    HourlyWeatherItem(
-                      time: "10:00",
-                      temperature: "32°C",
-                      iconPath: "assets/images/sunny.png",
-                    ),
-                  ],
-                ),
-              )
-            ],
+                  );
+                } catch (e) {
+                  return Center(child: Text('Error extracting data: $e'));
+                }
+              }
+            },
           ),
         ),
       ),
